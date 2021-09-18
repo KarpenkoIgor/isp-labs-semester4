08b3e918eda98781a3ba7e8d8fa40880db8f0ed2
IgorKarpenko <hv.karpenko@gmail.com>
2021-09-18 21:13:47 +0300

    Added order and checkout

--- Lab34/carservice/mainapp/views.py
+++ Lab34/carservice/mainapp/views.py
@@ -18,6 +18,8 @@ from .models import (
     CartProduct,
     )
 from .mixins import CategoryDetailMixin, CartMixin
+from .forms import OrderForm
+
 
 class BaseView(CartMixin, View):
     
@@ -133,3 +135,15 @@ class CartView(CartMixin, View):
             'categories': categories,
         }
         return render(request, 'cart.html', context)
+
+class CheckoutView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        form = OrderForm(request.POST or None)
+        context = {
+            'cart': self.cart,
+            'categories': categories,
+            'form': form
+        }
+        return render(request, 'checkout.html', context)
