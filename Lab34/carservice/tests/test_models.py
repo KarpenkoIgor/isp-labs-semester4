f0151978d15675fff64137e43292ba0e4e2aa2ee
IgorKarpenko <hv.karpenko@gmail.com>
2021-09-22 00:32:20 +0300

    Adden new tests to test_model

--- Lab34/carservice/tests/test_models.py
+++ Lab34/carservice/tests/test_models.py
@@ -1,6 +1,7 @@
 import pytest
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
+from decimal import Decimal
 
 from mainapp.models import (
     Manufacturer,
@@ -34,11 +35,20 @@ def image():
     return SimpleUploadedFile("tyre1_image.png", content=b'', content_type="image/png")
 
 @pytest.fixture
-def carpart(image, manufacturern, category):
+def carpart(image, manufacturer, category):
     return CarPart.objects.create(
         manufacturer=manufacturer, title="Part228", code="123", category=category,
         slug="test-slug", image=image, price="10.00"
         )
+
+@pytest.fixture
+def cart(customer, carpart):
+    return Cart.objects.create(owner=customer, total_cost=1)
+
+@pytest.fixture
+def cart_product(customer, carpart, cart):
+    return CartProduct.objects.create(user=customer,cart=cart, carpart=carpart, total_cost=Decimal(10.00))
+
 
 
 @pytest.mark.django_db
@@ -56,9 +66,27 @@ def test_category(category):
     assert Category.objects.first().slug == 'test-categ-slug'
 
 @pytest.mark.django_db
-def test_carpart(carpart):
+def test_carpart(carpart, category):
     assert CarPart.objects.count() == 1
     assert CarPart.objects.first() == carpart 
-    assert CarPart.objects.first().name == 'Part228' 
+    assert CarPart.objects.first().title == 'Part228' 
     assert CarPart.objects.first().slug == 'test-slug'
+    assert CarPart.objects.first().price == Decimal(10.00)
+    assert CarPart.objects.first().category == Category.objects.first()
+
+@pytest.mark.django_db
+def test_customer(customer, user):
+    assert Customer.objects.count() == 1
+    assert Customer.objects.first() == customer 
+    assert Customer.objects.first().user == user 
+    assert Customer.objects.first().phone == None
+    assert Customer.objects.first().user.email == customer.user.email
+    assert Customer.objects.first().phone == None
+    
+@pytest.mark.django_db
+def test_cart_product(cart_product, customer, cart):
+    assert CartProduct.objects.count() == 1
+    assert CartProduct.objects.first() == cart_product
+    assert CartProduct.objects.first().cart == cart
+
 
